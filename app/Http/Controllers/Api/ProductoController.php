6cad3bad54dcc35ff962d767bf0faa43113d3f04
chaskydev2 <chaskydev2@gmail.com>
2025-08-18 14:17:40 -0400

    frist api

---
+++ app/Http/Controllers/Api/ProductoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    public function index()
+    {
+        return Producto::all();
+    }
+
+    public function store(Request $request)
+    {
+        $producto = Producto::create($request->all());
+        return response()->json($producto, 201);
+    }
+
+    public function show($id)
+    {
+        return Producto::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::findOrFail($id);
+        $producto->update($request->all());
+        return response()->json($producto, 200);
+    }
+
+    public function destroy($id)
+    {
+        Producto::destroy($id);
+        return response()->json(null, 204);
+    }
+}
